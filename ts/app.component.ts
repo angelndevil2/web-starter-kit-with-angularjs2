83599666d30b4e665870f0f73e0261c4628d1dfc
k <jhkim@aimtog.co.kr>
2016-05-02 00:01:27 +0900

    apply upgradeElement

--- ts/app.component.ts
+++ ts/app.component.ts
@@ -1,9 +1,23 @@
 /**
  * Created by k on 16. 4. 30.
  */
-import {Component} from 'angular2/core';
+import {Component, Inject, ElementRef, OnInit} from 'angular2/core';
+declare var componentHandler: any;
+
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+    private elementRef;
+
+    constructor(@Inject(ElementRef) elementRef: ElementRef) {
+        this.elementRef = elementRef;
+    }
+
+    ngOnInit() {
+        // window.componentHandler.upgradeElement is provided by Material Design Lite
+        // and is necessary to call in order to "augment" dynamically added HTML
+        componentHandler.upgradeElement(this.elementRef.nativeElement.firstElementChild);
+    }
+}
